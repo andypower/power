55baec884d001b27e4c7883d8fcb7a8515dd9a31
Nazzareno Sileno <ssileno@tiscali.it>
2023-02-18 15:52:54 +0000

    indentation

--- combinazioneRegoli.py
+++ combinazioneRegoli.py
@@ -9,23 +9,23 @@ for z in range(10):
             for a in range(4):
               for b in range(3):
                 for c in range(2):
-            if i+j+k+x+y+z+a+b+c == 9:
-              lista = list()
-              lista.append(i)
-              lista.append(j)
-              lista.append(x)
-              lista.append(y)
-              lista.append(z)
-              lista.append(k)
-              lista.append(a)
-              lista.append(b)
-              lista.append(c)
-              lista.sort()
-              
-              listaPulita=[w for w in lista if w!=ele]
-              
-              stringa = ''.join(str(e) for e in listaPulita)
-              insieme.add(stringa)
+                  if i+j+k+x+y+z+a+b+c == 9:
+                    lista = list()
+                    lista.append(i)
+                    lista.append(j)
+                    lista.append(x)
+                    lista.append(y)
+                    lista.append(z)
+                    lista.append(k)
+                    lista.append(a)
+                    lista.append(b)
+                    lista.append(c)
+                    lista.sort()
+                    
+                    listaPulita=[w for w in lista if w!=ele]
+                    
+                    stringa = ''.join(str(e) for e in listaPulita)
+                    insieme.add(stringa)
 
               
 print('Elementi del set', len(insieme), insieme)
