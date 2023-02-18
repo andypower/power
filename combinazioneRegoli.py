1363f5b9c9e59953e10f55dd33a17eef61bd15ea
Nazzareno Sileno <ssileno@tiscali.it>
2023-02-18 16:35:41 +0100

    Update combinazioneRegoli.py
    9 comb

--- combinazioneRegoli.py
+++ combinazioneRegoli.py
@@ -1,12 +1,15 @@
 ele=0
 insieme = set()
-for z in range(7):
-  for y in range(7):  
-    for x in range(7):  
+for z in range(10):
+  for y in range(9):  
+    for x in range(8):  
       for k in range(7):  
-        for j in range(7):  
-          for i in range(7):  
-            if i+j+k+x+y+z == 6:
+        for j in range(6):  
+          for i in range(5):
+            for a in range(4):
+              for b in range(3):
+                for c in range(2):
+            if i+j+k+x+y+z+a+b+c == 9:
               lista = list()
               lista.append(i)
               lista.append(j)
@@ -14,6 +17,9 @@ for z in range(7):
               lista.append(y)
               lista.append(z)
               lista.append(k)
+              lista.append(a)
+              lista.append(b)
+              lista.append(c)
               lista.sort()
               
               listaPulita=[w for w in lista if w!=ele]
